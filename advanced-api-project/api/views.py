d104e255ada84bd7b6dc29c3e4fe10a353398a8d
Markmang <markudeagha@gmail.com>
2025-09-26 22:32:26 +0100

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions, filters
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+
+# List all books (with search + ordering)
+class BookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+    # Enable filtering
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'author__name']   # search by book title or author’s name
+    ordering_fields = ['title', 'publication_year']  # allow ordering by title/year
+    ordering = ['title']  # default ordering
+
+
+# Retrieve a single book by ID
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+# Create a new book (only for authenticated users)
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    # Automatically link logged-in user as the author
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user.author)
+
+
+# Update a book (only for authenticated users)
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+# Delete a book (only for authenticated users)
+class BookDeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
